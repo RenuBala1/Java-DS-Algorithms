2a6cd709ae2cc4b46dab05d55673308ea190cd05
RenuBala1 <33934585+RenuBala1@users.noreply.github.com>
2024-03-05 14:21:36 -0800

    Create CompressString.java

---
+++ src/warmup/CompressString.java
@@ -0,0 +1,14 @@
+package warmup;
+
+import java.util.Collections;
+
+public class CompressString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		String input="aaabbbcdaa";
+		
+		System.out.println("Compressed string after sorting :"+ StringUtilities.compresS(input));
+	}
+
+}
